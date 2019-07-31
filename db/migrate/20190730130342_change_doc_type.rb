7d15551e8d06b9cd01566083dc3b020ecbdeb9a6
Felix Pujols <pujols.fr@gmail.com>
2019-07-31 02:01:19 -0400

    0.44 Multiple changes to the customers views

--- db/migrate/20190730130342_change_doc_type.rb
+++ db/migrate/20190730130342_change_doc_type.rb
@@ -1,5 +1,5 @@
 class ChangeDocType < ActiveRecord::Migration[5.2]
   def change
-    change_column :customers, :doc_type, :integer
+    change_column :customers, :doc_type, :integer, using: 'doc_type::integer'
   end
 end
