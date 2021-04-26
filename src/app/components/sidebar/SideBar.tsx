c0ac6bf8337946e658f917a1487ba1f3569585f3
Felix Pujols <pujols.fr@gmail.com>
2021-04-26 10:51:57 -0400

    New style to SideBar & new colors for tailwind added

--- src/app/components/sidebar/SideBar.tsx
+++ src/app/components/sidebar/SideBar.tsx
@@ -1,54 +1,65 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Dropdown from './Dropdown';
+
 const SideBar = () => {
 	return (
-		<div>
-			<div>
-				<Dropdown />
+		<div className="relative min-h-screen flex">
+			<div className="bg-white text-blue w-64">
+				<Link
+					to="/"
+					className="text-base  block  transition duration-500 ease-in-out  hover:bg-glacier  transform"
+				>
+					LOGO
+				</Link>
+				<nav>
+					<div>
+						<Dropdown />
+					</div>
+					<ul className="flex  justify-center flex-col ">
+						<li className="">
+							<Link
+								to="/"
+								className="text-base  block  transition duration-500 ease-in-out  hover:bg-gray-500  transform pb-3 pt-3 pl-10"
+							>
+								Inicio
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="/Clients"
+								className="text-base  block  transition duration-500 ease-in-out  hover:bg-gray-500  transform pb-3 pt-3 pl-10"
+							>
+								Clientes
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="/Medicine"
+								className="text-base  block  transition duration-500 ease-in-out  hover:bg-gray-500  transform pb-3 pt-3 pl-10"
+							>
+								Medicamentos
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="/Configuration"
+								className="text-base  block  transition duration-500 ease-in-out  hover:bg-gray-500  transform pb-3 pt-3 pl-10"
+							>
+								Configuracion
+							</Link>
+						</li>
+						<li>
+							<Link
+								to="/Help"
+								className="text-base  block  transition duration-500 ease-in-out  hover:bg-gray-500  transform pb-3 pt-3 pl-10"
+							>
+								Ayuda
+							</Link>
+						</li>
+					</ul>
+				</nav>
 			</div>
-			<ul className="flex  justify-center flex-col ">
-				<li className="">
-					<Link
-						to="/"
-						className="text-base  block  transition duration-500 ease-in-out  hover:bg-gray-500  transform"
-					>
-						Inicio
-					</Link>
-				</li>
-				<li>
-					<Link
-						to="/Clients"
-						className="text-base  block  transition duration-500 ease-in-out  hover:bg-gray-500  transform"
-					>
-						Clientes
-					</Link>
-				</li>
-				<li>
-					<Link
-						to="/Medicine"
-						className="text-base  block  transition duration-500 ease-in-out  hover:bg-gray-500  transform"
-					>
-						Medicamentos
-					</Link>
-				</li>
-				<li>
-					<Link
-						to="/Configuration"
-						className="text-base  block  transition duration-500 ease-in-out  hover:bg-gray-500  transform"
-					>
-						Configuracion
-					</Link>
-				</li>
-				<li>
-					<Link
-						to="/Help"
-						className="text-base  block  transition duration-500 ease-in-out  hover:bg-gray-500  transform"
-					>
-						Ayuda
-					</Link>
-				</li>
-			</ul>
 		</div>
 	);
 };
